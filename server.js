5599f3eefe260ddabba85b92f486bb7113238886
Pedro Zago <pedroozago97@gmail.com>
2020-05-06 12:44:39 +0000

    Versão com o CRUD em memória

--- server.js
+++ server.js
@@ -2,8 +2,57 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+server.use(express.json());
+
+const filmes = [
+    {id: 1, nome: 'Clube da luta', diretor: 'David Fincher', ano: 1999, assistido: 'Sim'},
+    {id: 2, nome: 'Vingadores ', diretor: 'Joss Whedon', ano: 2012, assistido: 'Sim'}
+]
+
+server.get('/filme', function(request, response) {
+    response.json(filmes);
 })
+
+server.post('/filme', function(request, response) {
+
+    const {id, nome, diretor, ano, assistido} = request.body;
+
+    filmes.push({id, nome, diretor, ano, assistido})
+    response.status(204).send();
+})
+
+server.put('/filme/:id', function(request, response){
+    const id = request.params.id;
+    const {nome, diretor, ano, assistido} = request.body;
+
+    for(let i = 0; i < filmes.length; i++){
+        if(filmes[i].id == id) {
+            filmes[i].nome = nome;
+            filmes[i].diretor = diretor;
+            filmes[i].ano = ano;
+            filmes[i].assistido = assistido;
+            break;
+        }
+    }
+
+    return response.status(204).send();
+})
+
+server.delete('/filme/:id', function(request, response) {
+
+    const id = request.params.id;
+
+    for(let i = 0; i < filmes.length; i++){
+        if(filmes[i].id == id) {
+            filmes.splice(i, 1)
+            break;
+        }
+    }
+
+    return response.status(204).send();
+})
+
+
+
 
 server.listen(process.env.PORT || 3000);
